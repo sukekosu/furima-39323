9efc54012cd97d7fa523bc8638c17ca737d7a211
sukekosu <iesokkmkm@gmail.com>
2023-09-18 21:47:06 +0900

    空対策　バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :orders
+
+  validates :nickname, :family_name, :first_name, :kana_family_name, :kana_first_name, :birthday, presence: true
 end
