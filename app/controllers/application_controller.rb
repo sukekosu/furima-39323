159aa29a002e8f78adc331078fd86074ed371bbe
sukekosu <iesokkmkm@gmail.com>
2023-09-19 01:51:46 +0900

    ログインページ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-
   private
 
   def basic_auth
@@ -11,7 +9,7 @@ class ApplicationController < ActionController::Base
       username == 'admin' && password == '2222'
     end
   end
-  
+
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :kana_family_name, :kana_first_name, :birthday])
   end
