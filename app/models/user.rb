9c4606515b9e51b910c78d58cb160f4de6591da7
sukekosu <iesokkmkm@gmail.com>
2023-09-19 22:28:08 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,9 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
-  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
   validates_format_of :family_name, :first_name, with: NAME_REGEX, message: 'には全角(漢字・ひらがな・カタカナ)で入力してください'
 
-  KANA_NAME_REGEX = /\A[ァ-ン]+\z/.freeze
+  KANA_NAME_REGEX = /\A[ァ-ヶ一-]+\z/.freeze
   validates_format_of :kana_family_name, :kana_first_name, with: KANA_NAME_REGEX, message: 'には全角カタカナで入力してください'
 end
