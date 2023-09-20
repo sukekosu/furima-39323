992c97b90ce7be44b208caf2ded3a039ffb98225
sukekosu <iesokkmkm@gmail.com>
2023-09-20 22:40:23 +0900

    出品画面、Activehash設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  # 後ほど定義
-  # has_many :items
-  # has_many :orders
+  has_many :items
+  has_many :orders
 
   validates :nickname, :family_name, :first_name, :kana_family_name, :kana_first_name, :birthday, presence: true
 
