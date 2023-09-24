f3b0d8fa213b0eb8615dbc2ed97a0dbf16ca5168
sukekosu <iesokkmkm@gmail.com>
2023-09-24 22:34:14 +0900

    orderモデル編集

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,5 @@
 class Order < ApplicationRecord
-  class Purchase < ApplicationRecord
-    belongs_to :user
-    belongs_to :item
-  end
+  belongs_to :user
+  belongs_to :item
+  has_one :address
 end
